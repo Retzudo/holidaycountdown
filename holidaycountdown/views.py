186cb27070076b67b7e85a12b512f1f81b35c655
Retzudo <christoph.willinger@gmail.com>
2017-02-09 12:05:05 +0100

    JSON output

--- holidaycountdown/views.py
+++ holidaycountdown/views.py
@@ -1,5 +1,5 @@
 import arrow
-from flask import render_template
+from flask import render_template, jsonify
 
 from holidaycountdown import app, holidays_list
 from holidaycountdown.holidays import get_next_holiday
@@ -14,3 +14,18 @@ def index():
         holiday=next_holiday,
         human_date=arrow.get(next_holiday.holiday_date).humanize(locale='de_at')
     )
+
+
+@app.route('/next.json')
+def next_holiday():
+    holiday = get_next_holiday(holidays_list)
+    arrow_date = arrow.get(holiday.holiday_date)
+
+    return jsonify({
+        'date': holiday.holiday_date.isoformat(),
+        'name': holiday.name,
+        'humanized': {
+            'de_at': arrow_date.humanize(locale='de_at'),
+            'en_gb': arrow_date.humanize(locale='en_gb'),
+        },
+    })
